12d05125bccf4bdfc1120a4aaeb55813eda47b49
wsm <2445806031@qq.com>
2023-05-19 16:31:15 +0800

    保护模式 输出P

--- boot/run.sh
+++ boot/run.sh
@@ -1,5 +1,6 @@
-nasm -I include/ -o mbr.bin mbr3.S
-dd if=./mbr.bin of=/home/wsm/bochs/hd60M.img bs=512 count=1 conv=notrunc
+# 路径!!!  就用绝对路径
+nasm -I /home/wsm/bochs/include/ -o /home/wsm/bochs/boot/mbr.bin /home/wsm/bochs/boot/mbr.S
+dd if=/home/wsm/bochs/boot/mbr.bin of=/home/wsm/bochs/hd60M.img bs=512 count=1 conv=notrunc
 
-nasm -I include/ -o loader.bin loader.S
-dd if=./loader.bin of=/home/wsm/bochs/hd60M.img bs=512 count=1 seek=2 conv=notrunc
+nasm -I /home/wsm/bochs/include/ -o /home/wsm/bochs/boot/loader.bin /home/wsm/bochs/boot/loader.S
+dd if=/home/wsm/bochs/boot/loader.bin of=/home/wsm/bochs/hd60M.img bs=512 count=2 seek=2 conv=notrunc
